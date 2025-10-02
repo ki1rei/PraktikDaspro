a285ab8cc379bdc3ad3bd303dceb2ab0f13f237a
ki1rei <studyacctyara@gmail.com>
2025-10-02 21:03:04 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS14.java
+++ jobsheet5/ifCetakKRS14.java
@@ -12,6 +12,8 @@ public class ifCetakKRS14 {
         if (uktLunas){
             System.out.println("Pembayaran UKT Terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
         
     }
