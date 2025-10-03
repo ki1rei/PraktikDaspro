f33843b1afbf99bd3a4141fab548d3c810503ec7
ki1rei <studyacctyara@gmail.com>
2025-10-03 11:18:32 +0700

    Percobaan 3

--- jobsheet5/nestedUjianSkripsi14.java
+++ jobsheet5/nestedUjianSkripsi14.java
@@ -17,7 +17,7 @@ public class nestedUjianSkripsi14 {
         int bimbinganP2 = sc.nextInt();
         
         if (bebasKompen.equalsIgnoreCase("Ya")) {
-            if (bimbinganP1 >= 8 && bimbinganP2 < 4 ) {
+            if (bimbinganP1 >= 8 && bimbinganP2 >= 4 ) {
                 pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
             } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
                 pesan = "Gagal ! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
